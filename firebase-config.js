b0dadd2a8c6f27156c883e365a43f99240b4aa8e
dhruba-star <indiracgps@gmail.com>
2025-05-08 23:40:44 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDXzV_Hc6dlDjaOAE0ytnfGaYUTUI5XyTE",
   authDomain: "demoadmin-cdee0.firebaseapp.com",
   projectId: "demoadmin-cdee0",
-  storageBucket: "demoadmin-cdee0.firebasestorage.app",
+  storageBucket: "demoadmin-cdee0.appspot.com", // Fixed this from ".app" to ".com"
   messagingSenderId: "692280160109",
   appId: "1:692280160109:web:e32123854c8cca270cb464"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { auth, db };
+export { db, auth };
